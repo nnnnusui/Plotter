066d507eef43438a0bd074c700dcada0836005b5
nnnnusui <nnnnusui@gmail.com>
2020-07-02 17:14:15 +0900

    📝➕ Http server on [object Plotter]

--- src/main/scala/com/github/nnnnusui/plotter/Plotter.scala
+++ src/main/scala/com/github/nnnnusui/plotter/Plotter.scala
@@ -1,5 +1,24 @@
 package com.github.nnnnusui.plotter
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+
+import scala.io.StdIn
+
 object Plotter extends App {
-  println("Hello world.")
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.dispatcher
+
+  val bindingFuture = Http().bindAndHandle(Router.route, "localhost", 8080)
+
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+
 }
