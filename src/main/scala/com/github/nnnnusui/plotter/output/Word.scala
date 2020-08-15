b416b91bde79c7118cad957bfb1fe89e7b06c6f3
nnnnusui <nnnnusui@gmail.com>
2020-08-16 06:52:42 +0900

    ➕📄 output.Word [GetAll, Create]

---
+++ src/main/scala/com/github/nnnnusui/plotter/output/Word.scala
@@ -0,0 +1,14 @@
+package com.github.nnnnusui.plotter.output
+
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+
+
+sealed trait Word
+object Word extends DefaultJsonProtocol {
+  case class GetAll(value: Seq[(Int, String)]) extends Word
+  case class Create(id: Int) extends Word
+  object JsonFormat{
+    implicit lazy val getAllOutput: RootJsonFormat[GetAll] = jsonFormat1(GetAll)
+    implicit lazy val createOutput: RootJsonFormat[Create] = jsonFormat1(Create)
+  }
+}
