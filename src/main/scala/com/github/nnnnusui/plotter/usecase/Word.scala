562c99d2fc54255a77d9f5372681d3a6fb6b3be1
nnnnusui <nnnnusui@gmail.com>
2020-08-16 06:57:59 +0900

    ➕📄 usecase.Word [GetAll, Create]

---
+++ src/main/scala/com/github/nnnnusui/plotter/usecase/Word.scala
@@ -0,0 +1,17 @@
+package com.github.nnnnusui.plotter.usecase
+
+import com.github.nnnnusui.plotter.UsesDispatcher
+import com.github.nnnnusui.plotter.entity.{Word => Entity}
+import com.github.nnnnusui.plotter.input.{Word => Input}
+import com.github.nnnnusui.plotter.output.{Word => Output}
+import com.github.nnnnusui.plotter.repository.{Word => Repository}
+
+import scala.concurrent.Future
+
+trait Word {
+  this: Repository with UsesDispatcher =>
+  def use(input: Input.GetAll): Future[Output.GetAll] =
+    getAll.map(it=> Output.GetAll(it.map(it=> (it.id, it.value))))
+  def use(input: Input.Create): Future[Output.Create] =
+    create(Entity(0, input.value)).map(it=> Output.Create(it))
+}
